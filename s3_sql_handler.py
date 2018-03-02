7e4f06bef33bf22d5d593169392c19edaa29dd29
yifanli5 <yifanli5@asu.edu>
2018-03-02 02:31:22 -0700

    add files to handle s3 and timeline

---
+++ s3_sql_handler.py
@@ -0,0 +1,53 @@
+import sqlite3
+import json
+
+class SlideshowSqlHandler:
+    def __init__(self):
+        self.conn = sqlite3.connect('s3.db')
+
+    def insert_s3_if_needed(self, json_file_path):
+        json_file = open(json_file_path)
+        json_data = json.load(json_file)
+
+        self.conn.execute('CREATE TABLE IF NOT EXISTS SLIDESHOW (upload_time INT PRIMARY KEY NOT NULL, asset_url TEXT, text TEXT)')
+        slide_show_list = json_data['example_slideshow']
+        database_insert = []
+        for item in slide_show_list:
+            upload_time = item['upload_time']
+            cursor = self.conn.execute('SELECT upload_time from SLIDESHOW where upload_time=?', (upload_time,))
+            data = cursor.fetchone()
+            if data is None:
+                database_insert.append((upload_time, item['asset_url'], item['text']))
+        self.conn.executemany('INSERT INTO slideshow VALUES (?,?,?)', database_insert)
+        self.conn.commit()
+
+    def retrieve(self):
+        json_data = {}
+        slideshow = []
+        cursor = self.conn.execute('SELECT upload_time, asset_url, text from SLIDESHOW')
+        for row in cursor:
+            slideshow_entry = {}
+            slideshow_entry['upload_time'] = row[0]
+            slideshow_entry['asset_url'] = row[1]
+            slideshow_entry['text'] = row[2]
+            slideshow.append(slideshow_entry)
+
+        json_data['slideshow'] = slideshow
+
+        return json_data
+
+    def retrieve_after_timestamp(self, timestamp):
+        json_data = {}
+        slideshow = []
+        cursor = self.conn.execute('SELECT upload_time, asset_url, text from SLIDESHOW')
+        for row in cursor:
+            upload_time = row[0]
+            if upload_time >= timestamp:
+                slideshow_entry = {}
+                slideshow_entry['upload_time'] = row[0]
+                slideshow_entry['asset_url'] = row[1]
+                slideshow_entry['text'] = row[2]
+                slideshow.append(slideshow_entry)
+
+        json_data['slideshow'] = slideshow
+        return json_data
---
+++ timeline_sql_handler.py
@@ -0,0 +1,48 @@
+import sqlite3
+import json
+
+class TimelineSQLHandler:
+    def __init__(self):
+        self.conn = sqlite3.connect('timeline.db')
+
+    def insert_timeline_if_needed(self, json_file_path):
+        json_file = open(json_file_path)
+        json_data = json.load(json_file)
+        timelineArr = json_data["timeline"]
+
+        self.conn.execute('CREATE TABLE IF NOT EXISTS TIMELINE (dateTime TEXT PRIMARY KEY NOT NULL, name TEXT, description TEXT, media TEXT)')
+        for entry in timelineArr:
+            dateTime = entry["dateTime"]
+            cursor = self.conn.execute('SELECT dateTime from TIMELINE where dateTime=?', (dateTime,))
+            timelineData = cursor.fetchone()
+
+            if timelineData is None:
+                name = entry["name"]
+                description = entry["description"]
+                media = json.dumps(entry["media"])
+                self.conn.execute('INSERT INTO TIMELINE (dateTime, name, description, media) VALUES (?,?,?,?)',
+                             (dateTime, name, description, media))
+        self.conn.commit()
+
+
+    def retrieve_timeline(self):
+        json_data = {}
+        timeline = []
+        cursor = self.conn.execute("SELECT dateTime, name, description, media from TIMELINE")
+        for row in cursor:
+            timeline_entry = {}
+            timeline_entry["dateTime"] = row[0]
+            timeline_entry["name"] = row[1]
+            timeline_entry["description"] = row[2]
+            timeline_entry["media"] = json.loads(row[3])
+
+            timeline.append(timeline_entry)
+
+        json_data["timeline"] = timeline
+
+        return json_data
+
+    def exit(self):
+        self.conn.close()
+
+
